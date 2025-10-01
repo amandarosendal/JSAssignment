905e9d6aa3f32d0edacdc22468a5f099abfcd2ed
amandarosendal <amandarosendal@hotmail.com>
2025-10-01 10:16:12 +0200

    statements

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 console.log("hello world");
 console.log("be happy and stay positive");
+console.log("be calm and peaceful");
+console.log("be present and calm");
+console.log("be proud and comitted");
+

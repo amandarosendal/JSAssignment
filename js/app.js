08a8962d7eea8306c3edcb7c15614f0164db8978
amandarosendal <amandarosendal@hotmail.com>
2025-10-01 11:43:19 +0200

    uträkning

--- js/app.js
+++ js/app.js
@@ -3,4 +3,49 @@ console.log("be happy and stay positive");
 console.log("be calm and peaceful");
 console.log("be present and calm");
 console.log("be proud and comitted");
+//Detta ska ej printas
+/*
+console.log("everything is gonna be ok");
+ */
+const examplestring= "hello this is example";
+console.log(examplestring);
+
+let numberofbottles = 10
+console.log(numberofbottles);
+const line1 = "green bottles hanging on the wall";
+console.log(line1);
+let numberoflessbottles  = 9
+const line2= "and if one green bottle, should accidently fall, there will be";
+console.log(line2);
+console.log (numberoflessbottles);
+
+let number =4;
+number += 2;
+console.log(number);
+
+let x = 2+8;
+let y = 2+1;
+let z = 2+2;
+console.log(x);
+
+let gold =50;
+++gold;
+console.log(gold);
+
+let kmh = 72
+let ms = 1440
+let speedMs = 1440/72
+console.log(speedMs);
+
+let minutes = 3
+let seconds = 60
+let time = minutes * 60;
+console.log(time)
+
+
+
+
+
+
+
 
